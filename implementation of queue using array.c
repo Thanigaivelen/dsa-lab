aeadfa03a5e9a3bd9d3006e3939ff3e17f7c7f48
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-18 08:01:42 +0530

    updated array queue

--- implementation of queue using array.c
+++ implementation of queue using array.c
@@ -1,69 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct node {
-	int data;
-	struct node *next;
-};
-struct node *front = 0;
-struct node *rear =0;
-int x;
-
-void enq(int x){
-  
-  struct node *newnode;
-  newnode = (struct node*)malloc(sizeof(struct node));
-  if (front==0&&rear==0){
-    front = newnode;
-    rear= newnode;
-    newnode->data=x;
-    rear->next=0;
-  }
-  else{
-      rear->next =newnode;
-      newnode->data=x;
-      rear=newnode;
-      rear->next=0;
-      
-  }
-}
-
-void deq(){
-  if (front==0&&rear==0){
-    printf("\nqueue is empty");
-  }
-  else{
-  	struct node *temp;
-  	temp= front;
-  	front= front->next;
-  	free(temp);
-  }
-  printf("\ndequeue is success");
-}
-void display(){
-  struct node*temp;
-  temp=front;
-  if(front==0&&rear==0){
-    printf("\nqueue is empty");
-  }
-  else{
-    while(temp!=0){
-      printf("\n%d",temp->data);
-      temp=temp->next;
-    }
-  }
-}
+void enqueue(int *f, int *r, int *arr);
+void dequeue(int *f, int *r, int *arr);
+void display(int *f, int *r, int *arr);
 
 int main() {
-    enq(588);
-    enq(250);
-    enq(55);
-    enq(68);
-    enq(99);
-    display();
-    deq();
-    display();
-    printf("\nHello World\n");
-    return 0;
-} 
+	int choice, front = -1, rear = -1;
+	int arr[50];
+
+	while(choice != 4) {
+		printf("\nenqueue(1)/ dequeue(2)/ display(3) / quit(4): ");
+		scanf("%d", &choice);
+
+		switch(choice) {
+			case 1:
+				enqueue(&front, &rear, arr);
+				break;
+			case 2:
+				dequeue(&front, &rear, arr);
+				break;
+			case 3:
+				display(&front, &rear, arr);
+				break;
+			case 4:
+				printf("quiting the program\n");
+				break;
+			default:
+				printf("invalid choice\n");
+				break;
+		}
+	}	
+
+	return 0;
+}
+
+
+void enqueue(int *f, int *r, int *arr) {
+	int element;
+	int front = *f, rear = *r;
+
+	printf("enter element: ");
+	scanf("%d", &element);
+
+	if(front == -1 && rear == -1) {
+		arr[0] = element;
+		front = 0;
+		rear = 0;
+	}
+	else if (rear == 99) {
+		printf("overflow\n");
+		return;
+	} 
+	else {
+		rear++;
+		arr[rear] = element;
+	}
+	
+	*f = front; *r = rear;	// to update in main variables 
+}
+
+void dequeue(int *f, int *r, int *arr) {
+	int front = *f, rear = *r;
+
+	if(front == -1 && rear == -1) {
+		printf("underflow\n");
+		return;
+	}
+	
+	printf("element deleted: %d\n", arr[front]);
+	if(front == rear) {
+		front = -1; 
+		rear = -1;
+	}
+	else {
+		front++;
+	}
+
+	*f = front; *r = rear; 	// to update in main variables
+}
+
+void display(int *f, int *r, int *arr) {
+	int i, front = *f, rear = *r;
+
+	if(front == -1 && rear == -1) {
+		printf("empty queue\n");
+		return;
+	}
+
+	printf("\nprinting elements: \n");
+	printf("front -> ");
+	for(i = front; i <= rear; i++)
+	{
+		printf("%d -> ", arr[i]);
+	}
+	printf("rear\n");
+}
