78abcaa4732185f01930b1a120ddade391ad7c80
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-18 08:00:45 +0530

    updated linkedlist queue

--- implementation of queue using linked list.c
+++ implementation of queue using linked list.c
@@ -1,69 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct node {
-	int data;
+typedef struct node {
+	int num;
 	struct node *next;
-};
-struct node *front = 0;
-struct node *rear =0;
-int x;
+}node;
 
-void enq(int x){
-  
-  struct node *newnode;
-  newnode = (struct node*)malloc(sizeof(struct node));
-  if (front==0&&rear==0){
-    front = newnode;
-    rear= newnode;
-    newnode->data=x;
-    rear->next=0;
-  }
-  else{
-      rear->next =newnode;
-      newnode->data=x;
-      rear=newnode;
-      rear->next=0;
-      
-  }
-}
-
-void deq(){
-  if (front==0&&rear==0){
-    printf("\nqueue is empty");
-  }
-  else{
-  	struct node *temp;
-  	temp= front;
-  	front= front->next;
-  	free(temp);
-  }
-  printf("\ndequeue is success");
-}
-void display(){
-  struct node*temp;
-  temp=front;
-  if(front==0&&rear==0){
-    printf("\nqueue is empty");
-  }
-  else{
-    while(temp!=0){
-      printf("\n%d",temp->data);
-      temp=temp->next;
-    }
-  }
-}
+void enqueue(node *front, node *rear);
+void dequeue(node *front, node *rear);
+void display(node *front, node *rear);
 
 int main() {
-    enq(588);
-    enq(250);
-    enq(55);
-    enq(68);
-    enq(99);
-    display();
-    deq();
-    display();
-    printf("\nHello World\n");
-    return 0;
-} 
+	int choice;
+
+	node *front = (node*) malloc(sizeof(node));
+	node *rear = (node*) malloc(sizeof(node));
+	if(front == NULL || rear == NULL) {
+		printf("no memory available\n");
+		return 1;
+	}
+	front->next = NULL;
+	rear->next = NULL;
+
+	while(choice != 4) {
+		printf("\nenqueue(1)/ dequeue(2)/ display(3) / quit(4): ");
+		scanf("%d", &choice);
+
+		switch(choice) {
+			case 1:
+				enqueue(front, rear);
+				break;
+			case 2:
+				dequeue(front, rear);
+				break;
+			case 3:
+				display(front, rear);
+				break;
+			case 4:
+				printf("quiting the program\n");
+				break;
+			default:
+				printf("invalid choice\n");
+				break;
+		}
+	}	
+
+	return 0;
+}
+
+
+void enqueue(node *front, node *rear) {
+	node *newnode = (node*) malloc(sizeof(node));
+	if(newnode == NULL) {
+		printf("overflow\n");
+		return;
+	}
+
+	printf("enter element: ");
+	scanf("%d", &newnode->num);
+
+	if(front->next == NULL) {
+		front->next = newnode;
+		newnode->next = rear;
+		return;
+	}
+
+	node *temp = front->next;
+	while (temp->next != rear) {
+		temp = temp->next;
+	}
+
+	temp->next = newnode;
+	newnode->next = rear;
+}
+
+void dequeue(node *front, node *rear) {
+	node *first = front->next;
+	if(first == NULL) {
+		printf("underflow\n");
+		return;
+	}
+
+	printf("deleted element: %d\n", first->num);
+
+	front->next = first->next;
+	free(first);
+}
+
+void display(node *front, node *rear) {
+	node *temp = front->next;
+	if(temp == NULL) {
+		printf("empty queue\n");
+		return;
+	}
+
+	printf("printing elements: \n");
+	printf("front -> ");
+	while (temp != rear) {
+		printf("%d ", temp->num);
+		temp = temp->next;
+	}
+	printf("rear\n");
+}
