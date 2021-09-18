b1e83ed211a1fd6d02647e73f0a9b8dde60a8173
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-18 10:05:00 +0530

    Update selection sort program.c

--- selection sort program.c
+++ selection sort program.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
-int main() {
-   int arr[10]={6,12,0,18,11,99,55,45,34,2};
-   int n=10;
-   int i, j, position, swap;
-   for (i = 0; i < (n - 1); i++) {
-      position = i;
-      for (j = i + 1; j < n; j++) {
-         if (arr[position] > arr[j])
-            position = j;
-      }
-      if (position != i) {
-         swap = arr[i];
-         arr[i] = arr[position];
-         arr[position] = swap;
-      }
-   }
-   for (i = 0; i < n; i++)
-      printf("%d\t", arr[i]);
-   return 0;
-}
+int main()
+{
+  int array[100], n, c, d, position, t;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0; c < (n - 1); c++) // finding minimum element (n-1) times
+  {
+    position = c;
+
+    for (d = c + 1; d < n; d++)
+    {
+      if (array[position] > array[d])
+        position = d;
+    }
+    if (position != c)
+    {
+      t = array[c];
+      array[c] = array[position];
+      array[position] = t;
+    }
+  }
