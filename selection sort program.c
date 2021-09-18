35a94de215b8d318c19e2cb55e1e2d6ffffa424e
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-18 09:29:47 +0530

    Update selection sort program.c

--- selection sort program.c
+++ selection sort program.c
@@ -1,34 +1,21 @@
-#include<stdio.h>
-int main(){
-   /* Here i & j for loop counters, temp for swapping,
-    * count for total number of elements, number[] to
-    * store the input numbers in array. You can increase
-    * or decrease the size of number array as per requirement
-    */
-   int i, j, count, temp, number[25];
-
-   printf("How many numbers u are going to enter?: ");
-   scanf("%d",&count);
-
-   printf("Enter %d elements: ", count);
-   // Loop to get the elements stored in array
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
- 
-   // Logic of selection sort algorithm
-   for(i=0;i<count;i++){
-      for(j=i+1;j<count;j++){
-         if(number[i]>number[j]){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
+#include <stdio.h>
+int main() {
+   int arr[10]={6,12,0,18,11,99,55,45,34,2};
+   int n=10;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
       }
    }
-
-   printf("Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
-
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
    return 0;
 }
