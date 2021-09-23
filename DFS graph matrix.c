2f29c508a12b593df25a0c9b9b71bf07142d4f60
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-23 10:26:50 +0530

    Update DFS graph matrix.c

--- DFS graph matrix.c
+++ DFS graph matrix.c
@@ -1,40 +1,41 @@
 // Iplementation of graph using matrix
-#include <stdio.h> 
-
-int dfs(int x,int size);
-int graph[10][10],visited[10];
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+void DFS (int x ,int size);
+int G[20][20],visited[20];
+void main()
 {
-  int i,j,size;
-  printf("enter the size less then 10 \t");
-  scanf("%d",&size);
-  for(i=0;i<size;i++)
-  {
-    for(j=0;j<size;j++)
-      scanf("%d",&graph[i][j]);
-    visited[i]=0;
+    int i,j,size;
+    printf("ENTER THE SIZE OF THE MATRIX\n");
+    scanf("%d",&size);
+    printf("ENTER THE ADJACENCY OF THE MATRIX :\n");
+    for (i=0;i<size;i++)
+    {
+        for (j=0;j<size;j++)
+        {
+           scanf("%d",&G[i][j]);
+        }
+    }
 
-  }
-  printf("\nMatrix \n");
-  for(i=0;i<size;i++)
-  {
-    printf("\n");
-    for(j=0;j<size;j++)
-      printf(" %d ",graph[i][j]);
-  }
-  printf("\n\n");
-  dfs(0,size);
+    for (i=0;i<size;i++)
+    {
+        visited[i]=0;
+    }
 
+    DFS(0,size);
 }
-int dfs(int x,int size)
+void DFS(int x,int size)
 {
-  int j;
-  visited[x]=1;
-  printf("\n%d",x);
-  for(j=0;j<size;j++)
-  {
-    if(visited[j]!=1&&graph[x][j]==1)
-      dfs(j,size);
-  }
-   return 0;
+    int j;
+
+    printf("%d",x);
+
+    printf("\n");
+
+    visited[x]=1;
+
+    for(j=0;j<size;j++)
+
+       if(!visited[j]&&G[x][j]==1)
+        DFS(j,size);
 }
