e9b78d59880c4a571e19c32d0f9d7f72c34230b9
Thanigaivelen <90086803+Thanigaivelen@users.noreply.github.com>
2021-09-24 10:05:29 +0530

    Update topological-ordering.c

--- topological-ordering.c
+++ topological-ordering.c
@@ -1,46 +1,56 @@
-#include <stdio.h>
+#include<stdio.h>
+
+void topo (int G[20][20] ,int size);
+int visited[20];
 int main()
 {
-  //indeg-indegree
-  int tr[10][10],visited[10],indeg[10];
-  int i,j,k,size,count=1;
-  printf("Enter the Size of the matrix ");
-  scanf("%d",&size);
-  for(i=0;i<size;i++)
-  {
-   printf("Enter the %d row\n",i+1);
-    for(j=0;j<size;j++)
-    {
-      scanf("%d",&tr[i][j]);
-    }
-    
-  }
-  for(i=0;i<size;i++)
-  {
-    visited[i]=0;
-    indeg[i]=0;
-  }
-  for(i=0;i<size;i++)
-    for(j=0;j<size;j++)
-      indeg[i]=indeg[i]+tr[j][i];
-  printf("The topological Order \n");
-  while(count<=size)
-  {
-    for(k=0;k<size;k++)
-    {
-      if(indeg[k]==0&& (visited[k]==0))
-      {
-        printf(" %d ",k+1);
-        visited[k]=1;
-      }
-      for(i=0;i<size;i++)
-      {
-        if(tr[i][k]==1)
-        indeg[k]--;
-      }
-    }
-    count++;
-  }
-return 0;
+	int i, j, size;
+	int G[20][20];
+	printf("Enter the size of matrix: ");
+	scanf("%d",&size);
+	printf("Enter the adjacency of matrix:\n");
+	for (i=0;i<size;i++)
+	{
+		for (j=0;j<size;j++)
+		{
+			scanf("%d",&G[i][j]);
+		}
+	}
 
+	for (i=0;i<size;i++)
+	{
+		visited[i]=0;
+	}
+
+	printf("vertices according to topological sorting are: \n");
+  topo(G, size);
+
+  return 0;
+}
+
+void topo (int G[20][20],int size)
+{
+	// find zero pointing node 
+  for (int k = 0; k < size; k++) {
+    int count = 0, node = -1;
+    for (int i = 0; i < size; i++) {
+      count = 0;
+      for(int j = 0; j < size; j++) {
+        // printf("%d ", G[j][i]);
+        if(G[j][i] == 0 && !visited[i]) {
+          count++;
+        }
+      }
+      // printf("count: %d and i = %d\n", count, i);
+      if (count == size) {
+        node = i;
+        visited[node] = 1;
+        break;
+      }
+    }
+    printf("%d\n", node);
+
+    for (int i = 0; i < size; i++) 
+      G[node][i] = 0;
+  }
 }
